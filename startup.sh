580fc5ba24d05ccbf0b110d1aaadbef579ea5a19
VIZZARD-X <vigneshanandmay13@gmail.com>
2026-09-01 23:14:16 +0530

    Configure startup command to run migrations and seed DB on Azure

--- startup.sh
+++ startup.sh
@@ -2,5 +2,7 @@
 cd backend
 echo "Running database migrations..."
 python manage.py migrate
+echo "Seeding database..."
+python seed.py
 echo "Starting Gunicorn server..."
 gunicorn attend_backend.wsgi:application --bind=0.0.0.0:8000 --timeout 600
