a2b0fa2d8f3e8219415f6ea97a750ee5d0b518ab
VIZZARD-X <vigneshanandmay13@gmail.com>
2026-09-13 01:24:22 +0530

    Fix web package compilation error for android

---
+++ frontend/attendance_app/lib/utils/web_utils_stub.dart
@@ -0,0 +1,5 @@
+import 'dart:typed_data';
+
+void downloadWeb(Uint8List bytes, String filename) {
+  throw UnsupportedError('Cannot download on this platform');
+}
--- frontend/attendance_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/attendance_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,8 @@ import Foundation
 import app_links
 import connectivity_plus
 import file_picker
+import flutter_ble_peripheral
+import flutter_blue_plus_darwin
 import flutter_secure_storage_macos
 import mobile_scanner
 import share_plus
@@ -18,6 +20,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FlutterBlePeripheralPlugin.register(with: registry.registrar(forPlugin: "FlutterBlePeripheralPlugin"))
+  FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
